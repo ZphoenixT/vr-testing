ed13dfb92d3b38463d1288fae23484071a733f1f
Zander Toews <zptoews@gmail.com>
2024-06-26 11:04:49 -0500

    added files and code for hand models

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { VRCanvas, Hands, VRButton, XR } from '@react-three/xr'
 import { useThree, useFrame, Canvas } from '@react-three/fiber'
 import { Box, OrbitControls, Plane, Sphere, Sky, useMatcapTexture } from '@react-three/drei'
 import { usePlane, useBox, Physics, useSphere } from '@react-three/cannon'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 function App() {
   return (
@@ -19,6 +20,17 @@ function App() {
           friction: 0.09
           }}>
           <Scene />
+          
+          {/* Load left hand model */}
+          <GLTFLoader url="C:\Users\ztoew\Desktop\R3F Testing\my-app4\public\models\left.glb">
+              {(gltfLeft) => <primitive object={gltfLeft.scene} />}
+            </GLTFLoader>
+            
+            {/* Load right hand model */}
+            <GLTFLoader url="C:\Users\ztoew\Desktop\R3F Testing\my-app4\public\models\right.glb">
+              {(gltfRight) => <primitive object={gltfRight.scene} />}
+            </GLTFLoader>
+
           <Hands />
         </Physics>
       </XR>
