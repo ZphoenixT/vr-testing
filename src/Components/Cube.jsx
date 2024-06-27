bb9e448b3be872d302d942cc0c150d557d06133f
Zander Toews <zptoews@gmail.com>
2024-06-27 13:54:03 -0500

    added useSate

--- src/Components/Cube.jsx
+++ src/Components/Cube.jsx
@@ -1,6 +1,7 @@
 import { Box, useMatcapTexture } from '@react-three/drei'
 import { useBox, Physics, useSphere } from '@react-three/cannon'
 import { Interactive } from '@react-three/xr';
+import { useState } from 'react';
 
 function Cube({ position, args = [0.06, 0.06, 0.06] }) {
     const [boxRef] = useBox(() => ({
