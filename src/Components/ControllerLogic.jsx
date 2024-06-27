75827f2f0bae5ef5f3fb910825478c8e02b49c80
Zander Toews <zptoews@gmail.com>
2024-06-27 13:13:09 -0500

    idk what this even is anymorme

--- src/Components/ControllerLogic.jsx
+++ src/Components/ControllerLogic.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { Controllers, useXR, TeleportationPlane } from '@react-three/xr';
+import { Controllers, TeleportationPlane } from '@react-three/xr';
 import { useBox } from '@react-three/cannon';
 import { useFrame } from '@react-three/fiber';
 import { Box } from '@react-three/drei';
@@ -12,7 +12,7 @@ function ControllerLogic() {
   const [leftControllerCollisionRef, leftControllerApi] = useBox(() => ({
     args: [0.2, 0.2, 0.2],
     type: 'Dynamic',
-    position: [0, 0, 0], // Initial position, will be updated in useFrame
+    position: [0, 0, 0],
     collisionFilterGroup: 2,
     collisionFilterMask: 1,
   }));
@@ -20,43 +20,57 @@ function ControllerLogic() {
   const [rightControllerCollisionRef, rightControllerApi] = useBox(() => ({
     args: [0.2, 0.2, 0.2],
     type: 'Dynamic',
-    position: [0.5, 0, 0], // Initial position, will be updated in useFrame
+    position: [0.5, 0, 0],
     collisionFilterGroup: 3,
     collisionFilterMask: 1,
   }));
 
   // Update collision box positions and rotations based on controllers
   useFrame(() => {
-    if (leftControllerRef.current) {
+    if (leftControllerRef.current && leftControllerRef.current.controller) {
       const { position, rotation } = leftControllerRef.current.controller;
-      console.log('Left Controller Found:', position.clone().toArray());
+      console.log('Left Controller Position:', position.toArray());
+      console.log('Left Controller Rotation:', rotation.toArray());
       leftControllerApi.position.copy(position);
       leftControllerApi.rotation.copy(rotation);
-      console.log("Left Test");
+    } else {
+      console.log('Left Controller not connected or controller property is undefined.');
     }
 
-    if (rightControllerRef.current) {
+    if (rightControllerRef.current && rightControllerRef.current.controller) {
       const { position, rotation } = rightControllerRef.current.controller;
-      console.log('Right Controller Found:', position.clone().toArray());
+      console.log('Right Controller Position:', position.toArray());
+      console.log('Right Controller Rotation:', rotation.toArray());
       rightControllerApi.position.copy(position);
       rightControllerApi.rotation.copy(rotation);
-      console.log("Right Test");
+    } else {
+      console.log('Right Controller not connected or controller property is undefined.');
     }
-
-    
   });
 
   return (
     <group>
       <Controllers
-        {...(props) => {
-          const { leftController, rightController } = props;
-          if (leftController) leftControllerRef.current = leftController.controller;
-          if (rightController) rightControllerRef.current = rightController.controller;
-          return null;
+        onConnected={(event) => {
+          if (event.controller.handedness === 'left') {
+            console.log('Left Controller Connected:', event);
+            leftControllerRef.current = event;
+          } else if (event.controller.handedness === 'right') {
+            console.log('Right Controller Connected:', event);
+            rightControllerRef.current = event;
+          }
+        }}
+        onDisconnected={(event) => {
+          if (event.controller.handedness === 'left') {
+            console.log('Left Controller Disconnected:', event);
+            leftControllerRef.current = null;
+          } else if (event.controller.handedness === 'right') {
+            console.log('Right Controller Disconnected:', event);
+            rightControllerRef.current = null;
+          }
         }}
       />
-      <TeleportationPlane leftHand/>
+      <TeleportationPlane leftHand />
       {/* Display the collision boxes */}
       <Box ref={leftControllerCollisionRef} args={[0.2, 0.2, 0.2]}>
         <meshBasicMaterial color="red" wireframe />
