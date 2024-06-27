a79b5b269c6a031d0cda632c0c7adff202097503
Zander Toews <zptoews@gmail.com>
2024-06-27 15:32:37 -0500

    add the hover colors

--- src/Components/Ball.jsx
+++ src/Components/Ball.jsx
@@ -1,10 +1,17 @@
 import { Sphere, useMatcapTexture } from '@react-three/drei'
-import { useBox, Physics, useSphere } from '@react-three/cannon'
+import { Physics, useSphere } from '@react-three/cannon'
 import { Interactive } from '@react-three/xr';
 import { useState } from 'react';
 
 function Ball({ position, args = [0.06, 32, 32] }) {
-    const [ballRef] = useSphere(() => ({ position, mass: 1, args }))
+    const [ballRef] = useSphere(() => ({
+      position,
+      mass: 1,
+      args,
+      type: 'Dynamic'
+    }))
+
+
     const [tex] = useMatcapTexture('C7C0AC_2E181B_543B30_6B6270')
     const [hovered, setHovered] = useState(false);
   
@@ -14,7 +21,7 @@ function Ball({ position, args = [0.06, 32, 32] }) {
       onBlur={() => setHovered(false)}
     >
       <Sphere ref={ballRef} args={args} castShadow>
-        <meshMatcapMaterial attach="material" matcap={tex} />
+        <meshMatcapMaterial attach="material" matcap={tex} color={hovered ? '#35ffff' : '#ffffff'}/>
       </Sphere>
       </Interactive>
     )
